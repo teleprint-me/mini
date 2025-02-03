daf372ce4a1e2e9418d10d1df0849a17064e4231
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-03 14:22:14 -0500

    model(refactor): Follow the llama paper and implementation as a guide

--- mini/transformer/model.py
+++ mini/transformer/model.py
@@ -34,10 +34,16 @@ class RMSNorm(nn.Module):
     def __init__(self, dim: int, eps: float = 1e-6):
         super().__init__()
         self.eps = eps
-        self.weight = nn.Parameter(torch.ones(dim))
+        self.weight = nn.Parameter(torch.ones(dim))  # Gamma
+
+    def _norm(self, x: torch.Tensor) -> torch.Tensor:
+        # (B, Seq_Len, Dim) * (B, Seq_Len, 1) = (B, Seq_Len, Dim)
+        # rsqrt: 1 / sqrt(x)
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
+        # (Dim) * (B, Seq_Len, 1) = (B, Seq_Len, Dim)
+        return self.weight * self._norm(x.float()).type_as(x)
 
 
 class FeedForward(nn.Module):
