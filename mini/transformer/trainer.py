10aed24b67834c317dab8a6bf5be6bc865ff0263
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-04 02:11:56 -0500

    trainer(refactor): Simplify the training pipeline and offload responsibilities to object states

--- mini/transformer/trainer.py
+++ mini/transformer/trainer.py
@@ -4,15 +4,15 @@ Module: mini.transformer.trainer
 Description: Trainer for the MiniTransformer model.
 """
 
+import logging
+from typing import Union
+
 import torch
 from sentencepiece import SentencePieceProcessor
-from torch import device, nn
-from torch.optim import Optimizer
-from torch.optim.lr_scheduler import LRScheduler
 
+from mini.common.logger import get_logger
 from mini.data.set import MiniDataset
-from mini.transformer.checkpoint import MiniCheckpoint
-from mini.transformer.model import MiniConfig, MiniTransformer
+from mini.transformer.state import MiniState
 
 
 class MiniTrainer:
@@ -20,65 +20,52 @@ class MiniTrainer:
         self,
         processor: SentencePieceProcessor,
         dataset: MiniDataset,
-        model: MiniTransformer,
-        config: MiniConfig,
-        optimizer: Optimizer,
-        scheduler: LRScheduler,
-        criterion: nn.Module,
-        checkpoint: MiniCheckpoint,
-        device: device,
+        state: MiniState,
         verbose: bool = False,
     ):
         self.processor = processor
         self.dataset = dataset
-        self.model = model
-        self.config = config
-        self.optimizer = optimizer
-        self.scheduler = scheduler
-        self.criterion = criterion
-        self.checkpoint = checkpoint
-        self.device = device
-        self.verbose = verbose
+        self.state = state
         self.pad_id = max(self.processor.pad_id(), 0)  # Ensure pad_id is non-negative
+
+        log_level = logging.DEBUG if verbose else logging.INFO
+        self.logger = get_logger(name=self.__class__.__name__, level=log_level)
 
     def log_batch(
         self,
         epoch: int,
         num_epochs: int,
-        batch_idx: int,
+        batch: int,
         loss: torch.Tensor,
     ):
         """Logs loss & perplexity for each batch."""
-        perplexity = (
-            torch.exp(loss).item()
-            if isinstance(loss, torch.Tensor)
-            else torch.exp(loss)
-        )
-        print(
+        self.logger.debug(
             f"[Epoch: {epoch+1}/{num_epochs}] "
-            f"[Batch: {batch_idx}/{len(self.dataset)}] "
+            f"[Batch: {batch}/{len(self.dataset)}] "
             f"[Loss: {loss.item():.6f}] "
-            f"[Perplexity: {perplexity:.6f}]"
+            f"[Perplexity: {self.perplexity(loss):.6f}]"
         )
 
-    def log_epoch(self, epoch: int, total_loss: float):
+    def log_epoch(self, epoch: int, num_epochs: int, total_loss: float):
         """Logs total epoch loss, learning rate, and perplexity."""
-        avg_loss = total_loss / len(self.dataset)
-        print(
-            f"[Epoch: {epoch+1}] "
+        average_loss = self.average_loss(total_loss)
+        self.logger.info(
+            f"[Epoch: {epoch+1}/{num_epochs}] "
             f"[Total Loss: {total_loss:.4f}] "
-            f"[Avg Loss: {avg_loss:.4f}] "
-            f"[LR: {self.scheduler.get_last_lr()[0]:.8f}] "
-            f"[Perplexity: {torch.exp(torch.tensor(avg_loss)).item():.6f}]"
+            f"[Avg Loss: {average_loss:.4f}] "
+            f"[LR: {self.state.scheduler.get_last_lr()[0]:.8f}] "
+            f"[Perplexity: {self.perplexity(average_loss):.6f}]"
         )
 
-    def resume(self) -> None:
-        """Load checkpoint, restoring optimizer/scheduler if available."""
-        loaded = self.checkpoint.load()
-        if loaded[0] is not None:
-            self.model = loaded[0]
-        if loaded[1] is not None:
-            self.optimizer = loaded[1]
+    def average_loss(self, x: Union[float, torch.Tensor]) -> float:
+        if isinstance(x, torch.Tensor):
+            x = x.item()
+        return x / len(self.dataset)
+
+    def perplexity(self, x: Union[float, torch.Tensor]) -> float:
+        if isinstance(x, float):
+            x = torch.tensor(x)
+        return torch.exp(x).item()
 
     def mask(self, x: torch.Tensor) -> torch.Tensor:
         """Creates a mask for the input tensor to ignore pad tokens."""
@@ -86,7 +73,6 @@ class MiniTrainer:
         return (
             (x != self.pad_id).unsqueeze(1).unsqueeze(2).expand(-1, -1, x.size(1), -1)
         )
-        # print(f"Mask Shape: {mask.shape}, Mask Sample: {mask[0, :, :, :10]}") # Debug
 
     def train(
         self,
@@ -98,32 +84,35 @@ class MiniTrainer:
         if self.verbose:
             print("Starting training...")
 
-        self.resume()
+        self.state.load()
+        device = self.state.runtime.device_type()
 
-        # Ensure model is in train mode
-        self.model.train()
+        model = self.state.model.to(device)
+        model.train()
+
+        optimizer = self.state.optimizer
+        scheduler = self.state.scheduler
+        criterion = self.state.criterion
 
         for epoch in range(num_epochs):
             total_loss = 0
-            self.optimizer.zero_grad()
+            for batch, (x, y) in enumerate(self.dataset):
+                optimizer.zero_grad()
+                x, y = x.to(device), y.to(device)
 
-            for batch_idx, (x, y) in enumerate(self.dataset):
-                x, y = x.to(self.device), y.to(self.device)
-
-                logits = self.model(x, self.mask(x))
-                loss = self.criterion(logits.view(-1, logits.size(-1)), y.view(-1))
+                logits = model(x, self.mask(x))
+                loss = criterion(logits.view(-1, logits.size(-1)), y.view(-1))
                 loss = loss / grad_accum_steps  # Normalize loss for accumulation
 
                 loss.backward(retain_graph=True)
+                optimizer.step()  # temporarily block this for debugging
                 total_loss += loss.item() * grad_accum_steps
 
-                # self.optimizer.step() # temporarily block this for debugging
-
                 # Debug: Check if weights are updating
-                for name, param in self.model.named_parameters():
+                for name, param in model.named_parameters():
                     if param.requires_grad and param.grad is not None:
                         before = param.clone().detach()
-                        self.optimizer.step()  # Apply optimizer updates
+                        optimizer.step()  # Apply optimizer updates
                         after = param.clone().detach()
                         diff = torch.norm(after - before).item()
                         print(f"Weight update norm for {name}: {diff}")
@@ -132,14 +121,14 @@ class MiniTrainer:
                         break
 
                 if self.verbose:
-                    self.log_batch(epoch, num_epochs, batch_idx, loss)
+                    self.log_batch(epoch, num_epochs, batch, loss)
 
-            self.scheduler.step()  # Step LR scheduler **once per epoch**
-            self.log_epoch(epoch, total_loss)
+            scheduler.step()  # Step LR scheduler **once per epoch**
+            self.log_epoch(epoch, num_epochs, total_loss)
 
             # Save model periodically
             if (epoch + 1) % save_every == 0:
-                self.checkpoint.save(self.model)
+                self.state.save()
 
         if self.verbose:
             print("Training complete!")
