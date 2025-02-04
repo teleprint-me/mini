3805a63a628042d86f65f5115ad62f8cece00829
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-04 02:12:45 -0500

    train(refactor): Add enhanced pipeline to separate concerns between configuration and runtime

--- mini/transformer/train.py
+++ mini/transformer/train.py
@@ -4,51 +4,32 @@ Script: mini.transformer.train
 Description: Simple pre-training loop for text-to-text generation.
 """
 
-import random
-
-import torch
 from sentencepiece import SentencePieceProcessor
 
 from mini.common.args import TransformerArgs
 from mini.data.set import MiniJsonDataset, MiniTextDataset
-from mini.transformer.checkpoint import MiniCheckpoint
-from mini.transformer.model import MiniConfig, MiniTransformer
-from mini.transformer.optimizer import MiniOptimizer
+from mini.transformer.manager import (
+    CriterionConfig,
+    MiniManager,
+    OptimizerConfig,
+    SchedulerConfig,
+)
+from mini.transformer.model import MiniConfig, MiniRuntime
+from mini.transformer.state import MiniState
 from mini.transformer.trainer import MiniTrainer
-
-
-def seed_all(seed: int) -> None:
-    """Sets the random seed for reproducibility."""
-    random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-
-
-def get_device() -> torch.device:
-    """Automatically detects and returns the best available device."""
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-        return torch.device("cuda")
-    return torch.device("cpu")
-
 
 if __name__ == "__main__":
     # Parse arguments
     args = TransformerArgs("Mini Training Tool").parse_args("train")
-    # Set random seed
-    seed_all(args.seed)
-    # Load device
-    device = get_device()
 
-    # Load the model tokenizer
+    # Load runtime configuration
+    runtime = MiniRuntime(seed=args.seed)
+    runtime.seed_all()
+
+    # Load model tokenizer
     processor = SentencePieceProcessor(model_file=args.processor)
-    # Get the vocab size
-    vocab_size = processor.vocab_size()
-    # Get the pad id
-    pad_id = max(processor.pad_id(), 0)
 
-    # Setup PyTorch Dataset & DataLoader
+    # Dynamically load Dataset & DataLoader
     dataset = None
     if args.dataset.endswith(".json"):
         dataset = MiniJsonDataset(
@@ -71,39 +52,34 @@ if __name__ == "__main__":
 
     # Load Transformer Config
     config = MiniConfig(
-        vocab_size=vocab_size,
+        vocab_size=processor.vocab_size(),
         embed_dim=args.embed_dim,
         num_heads=args.num_heads,
         head_dim=args.head_dim,
         num_layers=args.num_layers,
         ff_dim=args.ff_dim,
         max_seq_len=args.max_seq_len,
-        pad_id=pad_id,
+        pad_id=max(processor.pad_id(), 0),
         eps=args.eps,
         theta=args.rope_theta,
         bias=args.bias,
     )
 
-    # Initialize the model and move it to the specified device
-    model = MiniTransformer(config=config).to(device=device)
-
-    # Initialize the optimizer
-    optimizer = MiniOptimizer.create_optimizer(
-        model=model,
-        optimizer=args.optimizer,
+    # Load optimizer config
+    optimizer_config = OptimizerConfig(
+        type=args.optimizer,
         lr=args.lr,
         eps=args.eps,
-        weight_decay=args.weight_decay,
         amsgrad=args.amsgrad,
+        weight_decay=args.weight_decay,
         momentum=args.momentum,
         dampening=args.dampening,
         nesterov=args.nesterov,
     )
 
-    # Initialize the scheduler
-    scheduler = MiniOptimizer.create_scheduler(
-        optimizer=optimizer,
-        scheduler=args.scheduler,
+    # Load scheduler config
+    scheduler_config = SchedulerConfig(
+        type=args.scheduler,
         step_size=args.step_size,
         gamma=args.gamma,
         t_max=args.t_max,
@@ -112,29 +88,33 @@ if __name__ == "__main__":
         total_iters=args.total_iters,
     )
 
-    # Initialize the criterion
-    criterion = MiniOptimizer.create_criterion(args.criterion, pad_id=pad_id)
+    # Load criterion config
+    criterion_config = CriterionConfig(
+        type=args.criterion,
+        ignore_index=max(processor.pad_id(), 0),
+        reduction=args.reduction,
+    )
 
-    # Initialize the checkpoint
-    checkpoint = MiniCheckpoint(
+    # Load optimization manager
+    manager = MiniManager(
+        optimizer=optimizer_config,
+        scheduler=scheduler_config,
+        criterion=criterion_config,
+    )
+
+    # Load state manager
+    state = MiniState(
         path=args.model,
         config=config,
-        optimizer=optimizer,
-        device=device,
-        verbose=args.verbose,
+        manager=manager,
+        runtime=runtime,
     )
 
     # Load trainer
     trainer = MiniTrainer(
         processor=processor,
         dataset=dataset,
-        model=model,
-        config=config,
-        optimizer=optimizer,
-        scheduler=scheduler,
-        criterion=criterion,
-        checkpoint=checkpoint,
-        device=device,
+        state=state,
         verbose=args.verbose,
     )
 
