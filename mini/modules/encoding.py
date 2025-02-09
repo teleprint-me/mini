cb0273a12873a4330d7cbb1ae8a7776b0bc5b51c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-09 18:20:20 -0500

    encoding(refactor): Add base class to abstract and centralize common parameters and methods

--- mini/modules/encoding.py
+++ mini/modules/encoding.py
@@ -12,90 +12,88 @@ from torch import nn
 from mini.config import ConfigTransformer
 
 
-def _generate_sinusoidal_encoding(max_seq_len: int, embed_dim: int) -> torch.Tensor:
-    """Creates sinusoidal positional encodings as described in Vaswani et al. (2017)."""
-    pe = torch.zeros(max_seq_len, embed_dim)
-    position = torch.arange(max_seq_len, dtype=torch.float32).unsqueeze(1)
-    div_term = torch.exp(
-        torch.arange(0, embed_dim, 2, dtype=torch.float32)
-        * (-torch.log(torch.tensor(10000.0)) / embed_dim)
-    )
-    pe[:, 0::2] = torch.sin(position * div_term)
-    pe[:, 1::2] = torch.cos(position * div_term)
-    # NOTE: Ensure that PE is of shape (batch_size, max_seq_len, embed_dim)
-    # This must match the input shape of the attention mechanism.
-    return pe.unsqueeze(0)  # (b, l_max, d_e)
+class BaseEncoding(nn.Module):
+    """Base class for encoding blocks."""
+
+    def __init__(self, config: ConfigTransformer):
+        super().__init__()
+        self.config = config
+        self.dropout = nn.Dropout(self.config.dropout)
+
+    # Generate sinusoidal encoding
+    def _generate_sinusoidal_encoding(self) -> torch.Tensor:
+        """Creates sinusoidal positional encodings as described in Vaswani et al. (2017)."""
+        pe = torch.zeros(self.config.max_seq_len, self.config.embed_dim)
+        position = torch.arange(
+            self.config.max_seq_len, dtype=self.config.dtype
+        ).unsqueeze(1)
+        position = position.unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, self.config.embed_dim, 2, dtype=self.config.dtype)
+            * (-torch.log(torch.tensor(10000.0)) / self.config.embed_dim)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        return pe.unsqueeze(0)  # (batch_size, seq_len, embed_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Add positional encoding to input tensor x."""
+        raise NotImplementedError("Forward method must be implemented by subclasses.")
 
 
 # arxiv: arXiv:1803.02155
-class PositionalEncoding(nn.Module):
+class PositionalEncoding(BaseEncoding):
     """Standard fixed sinusoidal positional encoding for transformer models."""
 
     def __init__(self, config: ConfigTransformer):
-        super().__init__()
-        self.embed_dim = config.embed_dim
-        self.max_seq_len = config.max_seq_len
-        self.dropout = nn.Dropout(config.dropout)
-
+        super().__init__(config)
         # Register precomputed positional encodings as a non-trainable buffer
-        self.register_buffer(
-            "pe", _generate_sinusoidal_encoding(self.max_seq_len, self.embed_dim)
-        )
+        self.register_buffer("pe", self._generate_sinusoidal_encoding())
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Add positional encoding to input tensor x."""
-        return self.dropout(x + self.pe[:, : x.size(1), :])
+        seq_len = x.size(1)
+        return self.dropout(x + self.pe[:, :seq_len, :])
 
 
 # arxiv:1810.04805
-class BertEncoding(nn.Module):
+class BertEncoding(BaseEncoding):
     """Bert-style positional encoding."""
 
     def __init__(self, config: ConfigTransformer):
-        super().__init__()
-        self.embed_dim = config.embed_dim
-        self.max_seq_len = config.max_seq_len
-        self.dropout = nn.Dropout(config.dropout)
+        super().__init__(config)
         # Simple element-wise addition with nn.Parameter() for trainable parameters is sufficient.
-        self.pe = nn.Parameter(
-            data=_generate_sinusoidal_encoding(self.max_seq_len, self.embed_dim)
-        )
+        self.pe = nn.Parameter(data=self._generate_sinusoidal_encoding())
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Add positional encoding to input tensor x."""
-        return self.dropout(x + self.pe[:, : x.size(1), :])
+        seq_len = x.size(1)
+        return self.dropout(x + self.pe[:, :seq_len, :])
 
 
 # Projection layers for learnable compression and expansion
 # NOTE: This is experimental and can be substituted with other methods.
 # WARN: Training will oscillate if not properly tuned and model will be trapped in a local minimum.
 # This is due to the fact that the positional encodings are distorted by the learned projection.
-class LinearEncoding(nn.Module):
+class LinearEncoding(BaseEncoding):
     """Low-rank learned projection of sinusoidal positional encodings."""
 
     def __init__(self, config: ConfigTransformer):
-        super().__init__()
-        self.embed_dim = config.embed_dim
-        self.max_seq_len = config.max_seq_len
-        # Align with the number of heads to ensure compatibility with multi-head attention.
-        self.head_dim = self.embed_dim // config.num_heads
-        self.dropout = nn.Dropout(config.dropout)
-
+        super().__init__(config)
         # Register sinusoidal encoding as a non-trainable buffer
-        self.register_buffer(
-            "pe", _generate_sinusoidal_encoding(self.max_seq_len, self.embed_dim)
-        )
+        self.register_buffer("pe", self._generate_sinusoidal_encoding())
 
         # TODO: Evaluate the effectiveness of this approach.
         self.projection = nn.Sequential(
-            nn.Linear(self.embed_dim, self.head_dim),
+            nn.Linear(config.embed_dim, config.head_dim),
             nn.SiLU(),
-            nn.Linear(self.head_dim, self.embed_dim),
+            nn.Linear(config.head_dim, config.embed_dim),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Adds learned positional encoding to the input tensor."""
-        projected_pe = self.projection(self.pe[:, : x.size(1), :])
+        seq_len = x.size(1)
+        projected_pe = self.projection(self.pe[:, :seq_len, :])
         return self.dropout(projected_pe) + x
 
 
@@ -106,15 +104,11 @@ class RotaryEncoding(nn.Module):
 
     def __init__(self, config: ConfigTransformer):
         super().__init__()
-        self.embed_dim = config.embed_dim
-        self.max_seq_len = config.max_seq_len
-        assert self.embed_dim % 2 == 0, "Embedding dimension must be even for RoPE."
-
         # Precompute the rotation angles for RoPE
         theta = config.rope_theta ** (
-            -torch.arange(0, self.embed_dim, 2).float() / self.embed_dim
+            -torch.arange(0, config.embed_dim, 2).float() / config.embed_dim
         )
-        position = torch.arange(self.max_seq_len, dtype=torch.float32).unsqueeze(1)
+        position = torch.arange(config.max_seq_len, dtype=config.dtype).unsqueeze(1)
         angles = position * theta.unsqueeze(0)
         self.register_buffer("cos", torch.cos(angles))
         self.register_buffer("sin", torch.sin(angles))
