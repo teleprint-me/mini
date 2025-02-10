0b4b9ba189b11cf86d000b12e201d07ec64f5b02
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-10 03:45:38 -0500

    transformer(feat): Add support for position-wise and gated blocks

--- mini/modules/transformer.py
+++ mini/modules/transformer.py
@@ -14,28 +14,36 @@ from mini.modules.normalization import RMSNorm
 
 
 class PositionWiseBlock(nn.Module):
+    """Standard Transformer Block using Position-Wise FFN and Self-Attention."""
+
     def __init__(self, config: ConfigTransformer):
         super().__init__()
         self.attn = SelfAttention(config)
-        self.norm1 = LayerNorm()
-        self.norm2 = LayerNorm()
+        self.norm1 = LayerNorm(normalized_shape=config.embed_dim)
+        self.norm2 = LayerNorm(normalized_shape=config.embed_dim)
         self.ff = PositionWiseFeedForward(config)
+        self.dropout = nn.Dropout(config.dropout)  # Dropout before residuals
 
-    def forward(self, x):
-        x = self.norm1(x + self.attn(x))
-        x = self.norm2(x + self.ff(x))
+    def forward(self, x, mask=None):
+        """Forward pass through the transformer block."""
+        x = self.norm1(x + self.dropout(self.attn(x, mask)))
+        x = self.norm2(x + self.dropout(self.ff(x)))
         return x
 
 
 class GatedBlock(nn.Module):
+    """Transformer Block using Gated Feed-Forward Network and Rotary Attention."""
+
     def __init__(self, config: ConfigTransformer):
         super().__init__()
         self.attn = RotaryAttention(config)
-        self.norm1 = RMSNorm(config)
-        self.norm2 = RMSNorm(config)
+        self.norm1 = RMSNorm(config=config)
+        self.norm2 = RMSNorm(config=config)
         self.ff = GatedFeedForward(config)
+        self.dropout = nn.Dropout(config.dropout)
 
-    def forward(self, x):
-        x = self.norm1(x + self.attn(x))
-        x = self.norm2(x + self.ff(x))
+    def forward(self, x, mask=None):
+        """Forward pass through the transformer block."""
+        x = self.norm1(x + self.dropout(self.attn(x, mask)))
+        x = self.norm2(x + self.dropout(self.ff(x)))
         return x
