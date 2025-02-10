f25d165d9ece7166fa2fc00d849cd5f9e7d6c86f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-10 16:11:48 -0500

    misty(feat): Add MistyModel as a positional transformer, vaswani et al

--- mini/models/misty.py
+++ mini/models/misty.py
@@ -12,39 +12,47 @@ It is a simplified version of the transformer architecture, suitable for beginne
 to understand the basics of neural network design and training.
 """
 
+import torch
 import torch.nn as nn
 
 from mini.config import ConfigTransformer
+from mini.modules.attention import AttentionMask
 from mini.modules.embedding import PositionalEmbedding
-from mini.modules.hidden import HiddenBlock
-from mini.modules.normalization import RMSNorm
+from mini.modules.transformer import PositionWiseBlock
 
 
-# Wondering what a good name would be for this class.
-# MistyTransformer? or something else? Could use MistyModel or MistyNet.
-class MistyTransformer(nn.Module):
+class MistyModel(nn.Module):
+    """Minimal positional transformer model with self-attention and FFN layers."""
+
     def __init__(self, config: ConfigTransformer):
         super().__init__()
         self.bias = config.bias
 
+        # Mask is programmatically moved to the correct device and dtype defined by the config
+        self.mask = AttentionMask(config)
         self.embedding = PositionalEmbedding(config)
         self.blocks = nn.ModuleList(
-            [HiddenBlock(config) for _ in range(config.num_layers)]
+            [PositionWiseBlock(config) for _ in range(config.num_layers)]
         )
-        self.norm = RMSNorm(config)
+        self.norm = nn.LayerNorm(normalized_shape=config.embed_dim)
         self.head = nn.Linear(config.embed_dim, config.vocab_size)
 
         self.max_seq_len = config.max_seq_len
         self._init_weights()
 
     def _init_weights(self):
+        """Initialize model weights."""
         nn.init.xavier_uniform_(self.head.weight)
-        if self.bias is not None:
-            nn.init.uniform_(self.head.bias)
+        if self.bias:
+            nn.init.uniform_(self.head.bias, a=-0.1, b=0.1)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor, mask_type: str = "causal") -> torch.Tensor:
+        """Forward pass through Misty transformer."""
+        # Combine padding and attention masks
+        # NOTE: # This must happen before the embeddings layer
+        mask = self.mask(x, mask_type=mask_type)
         x = self.embedding(x)
         for block in self.blocks:
-            x = block(x)
+            x = block(x, mask)
         x = self.norm(x)
         return self.head(x)
