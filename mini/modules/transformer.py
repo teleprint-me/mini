c4b0c5d78db86d05dca7059fedd2a575b890987b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-10 03:36:57 -0500

    transformer(feat): Add initial rough sketches for handling position-wise and gated blocks

--- mini/modules/transformer.py
+++ mini/modules/transformer.py
@@ -5,20 +5,35 @@ Description: Contains the hidden transformer model components.
 """
 
 import torch.nn as nn
+from torch.nn import LayerNorm
 
 from mini.config import ConfigTransformer
-from mini.modules.attention import MultiHeadAttention
-from mini.modules.feed_forward import FeedForward
+from mini.modules.attention import RotaryAttention, SelfAttention
+from mini.modules.feed_forward import GatedFeedForward, PositionWiseFeedForward
 from mini.modules.normalization import RMSNorm
 
 
-class TransformerBlock(nn.Module):
+class PositionWiseBlock(nn.Module):
     def __init__(self, config: ConfigTransformer):
         super().__init__()
-        self.attn = MultiHeadAttention(config)
+        self.attn = SelfAttention(config)
+        self.norm1 = LayerNorm()
+        self.norm2 = LayerNorm()
+        self.ff = PositionWiseFeedForward(config)
+
+    def forward(self, x):
+        x = self.norm1(x + self.attn(x))
+        x = self.norm2(x + self.ff(x))
+        return x
+
+
+class GatedBlock(nn.Module):
+    def __init__(self, config: ConfigTransformer):
+        super().__init__()
+        self.attn = RotaryAttention(config)
         self.norm1 = RMSNorm(config)
         self.norm2 = RMSNorm(config)
-        self.ff = FeedForward(config)
+        self.ff = GatedFeedForward(config)
 
     def forward(self, x):
         x = self.norm1(x + self.attn(x))
