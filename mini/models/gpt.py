2685feaa9655d328b1dc7163c5ad8425e0638ecf
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-03-01 15:06:44 -0500

    gpt(patch): Rename from n_layers to num_layers

--- mini/models/gpt.py
+++ mini/models/gpt.py
@@ -126,7 +126,7 @@ class GPTModel(nn.Module):
         for pn, p in self.named_parameters():
             if pn.endswith("c_proj.weight"):
                 torch.nn.init.normal_(
-                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer)
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.num_layers)
                 )
 
     def _init_weights(self, module):
